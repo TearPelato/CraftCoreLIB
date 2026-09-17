a4a6d73be4b1f16464ea6aaa4e962d17fee5e642
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-09-17 21:33:17 +0200

    Added all the useful registries

--- common/src/main/java/net/tearpelato/craftcorelib/api/registry/ObjectRegistries.java
+++ common/src/main/java/net/tearpelato/craftcorelib/api/registry/ObjectRegistries.java
@@ -3,9 +3,16 @@ package net.tearpelato.craftcorelib.api.registry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -66,25 +73,66 @@ public class ObjectRegistries<T> {
         }
     }
 
-    public static class Registrar {
+    /**
+     * Creating a custom block with proper Item, getting the Id, and the supplier function to define the Block Properties
+     * */
 
-        /**
-         * Creating a custom block with proper Item, getting the Id, and the supplier function to define the Block Properties
-         * */
-
-        public static <T extends Block> ObjectRegistries<T> registerBlock(ResourceLocation id, Supplier<T> supplier) {
-            return new BlockRegistries<>(BuiltInRegistries.BLOCK, id, supplier, t -> new BlockItem(t, new Item.Properties()));
-        }
-
-        /**
-         * Creating a custom Item with the Id and the supplier for the Item Properties
-         * */
-
-        public static <T extends Item> ObjectRegistries<T> registerItem(ResourceLocation id, Supplier<T> supplier) {
-            return new ObjectRegistries<>(BuiltInRegistries.ITEM, id, supplier);
-        }
+    public static <T extends Block> ObjectRegistries<T> registerBlock(ResourceLocation id, Supplier<T> supplier) {
+        return new BlockRegistries<>(BuiltInRegistries.BLOCK, id, supplier, t -> new BlockItem(t, new Item.Properties()));
     }
 
+    /**
+     * Creating a custom Item with the Id and the supplier for the Item Properties
+     * */
+
+    public static <T extends Item> ObjectRegistries<T> registerItem(ResourceLocation id, Supplier<T> supplier) {
+        return new ObjectRegistries<>(BuiltInRegistries.ITEM, id, supplier);
+    }
+
+    public static ObjectRegistries<CreativeModeTab> registerCreativeTab(ResourceLocation id, Supplier<CreativeModeTab> supplier) {
+        return new ObjectRegistries<>(BuiltInRegistries.CREATIVE_MODE_TAB, id, supplier);
+    }
+
+    public static ObjectRegistries<BlockEntityType<?>> registerBlockEntity(ResourceLocation id, Supplier<BlockEntityType<?>> supplier) {
+        return new ObjectRegistries<>(BuiltInRegistries.BLOCK_ENTITY_TYPE, id, supplier);
+    }
+
+    public static ObjectRegistries<EntityType<?>> registerEntity(ResourceLocation id, Supplier<EntityType<?>> supplier) {
+        return new ObjectRegistries<>(BuiltInRegistries.ENTITY_TYPE, id, supplier);
+    }
+
+    public static ObjectRegistries<MenuType<?>> registerMenu(ResourceLocation id, Supplier<MenuType<?>> supplier) {
+        return new ObjectRegistries<>(BuiltInRegistries.MENU, id, supplier);
+    }
+
+    public static ObjectRegistries<RecipeType<?>> registerRecipeType(ResourceLocation id) {
+        return new ObjectRegistries<>(BuiltInRegistries.RECIPE_TYPE, id, ()-> new RecipeType<>() {
+            @Override
+            public String toString() {
+                return id.toString();
+            }
+        });
+    }
+
+    public static ObjectRegistries<RecipeSerializer<?>> registerRecipeSerializer(ResourceLocation id, Supplier<RecipeSerializer<?>> supplier) {
+        return new ObjectRegistries<>(BuiltInRegistries.RECIPE_SERIALIZER, id, supplier);
+    }
+
+    public static ObjectRegistries<SoundEvent> registerSound(ResourceLocation id, Supplier<SoundEvent> supplier) {
+        return new ObjectRegistries<>(BuiltInRegistries.SOUND_EVENT, id, supplier);
+    }
+
+    public static <T> ObjectRegistries<T> registerCustom(Registry<? super T> registry,ResourceLocation id, Supplier<T> supplier){
+        return new ObjectRegistries<>(registry, id, supplier);
+    }
+
+
+
+
+
+
+
+    //REGISTRATION HANDLER
     //Fabric Side
     public static void createAll(String modId) {
         List<ObjectRegistries<?>> entries = ENTRIES.get(modId);
